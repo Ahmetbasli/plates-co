e4513e19f399dfe4699da4415e5330e07318e297
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-20 16:39:22 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import "./index.scss";
 
+import initShoppingCart from "./utils/initShoppingCart";
+
+import ProductFeed from "./components/productFeed/productFeed";
+import Cart from "./components/cart/cart";
+import Header from "./components/header/header";
+
+initShoppingCart();
+const header = new Header();
+const productFeed = new ProductFeed();
+const cart = new Cart();
+
+header.apply();
+productFeed.apply();
+cart.apply();
