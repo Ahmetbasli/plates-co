af2c68b0b6bdddd0556c0666d4f17a591903b836
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-20 18:17:06 +0300

    Update empty cart page

--- src/components/cart/helpers/showCartEmptyPageIfCartEmpty.js
+++ src/components/cart/helpers/showCartEmptyPageIfCartEmpty.js
@@ -3,7 +3,7 @@ import { shoppingCart } from "../../../utils/initShoppingCart";
 const showCartEmptyPageIfCartEmpty = () => {
   if (shoppingCart.items.length === 0) {
     document.querySelector(".cart").style.display = "none";
-    document.querySelector(".cartEmptyPage").style.display = "block";
+    document.querySelector(".cartEmptyPage").style.display = "flex";
     return;
   }
   document.querySelector(".cartEmptyPage").style.display = "none";
