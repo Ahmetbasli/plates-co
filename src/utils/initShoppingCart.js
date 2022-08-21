3d901aeffaa5de4ea23f08b244b0e01d591a0d61
Ahmetbasli <ahmet.basli.mec@gmail.com>
2022-08-21 12:19:04 +0300

    Refactor init shopping cart

--- src/utils/initShoppingCart.js
+++ src/utils/initShoppingCart.js
@@ -1,10 +1,7 @@
 import ShoppingCart from "../models/shopping/shoppingCart";
 export let shoppingCart;
-export let refreshShoppingCartInDom;
-export let refreshCartBadge;
+
 const initShoppingCart = () => {
-  refreshShoppingCartInDom = new CustomEvent("refreshShoppingCartContent");
-  refreshCartBadge = new CustomEvent("refreshCartBadge");
   shoppingCart = new ShoppingCart();
   shoppingCart.setDeliveryCost(4.95);
   return shoppingCart;
